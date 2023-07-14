6b875e94e1034ca5378a72822c91e7a933d754f8
Majhrs16 <majhrs16.7@gmail.com>
2023-07-13 23:17:41 -0400

    b1.2.3

--- src/versions/J12/J12.py
+++ src/versions/J12/J12.py
@@ -1,4 +1,5 @@
 from os.path import join
 class Data(Data):
-	Natives = join(Data.MC, 'versions', Data.Version, 'natives')
+	Natives     = join(Data.MC, 'versions', Data.Version, 'natives')
+	AssetsIndex = '1.12'
 Data.Flags.JVM[-1] = '-Dlog4j.configurationFile=%s' % join(f'{Data.Lib}', 'client-1.12.xml')
